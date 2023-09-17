c99525dfe5264b620069bcdf1d7c0ea102eca445
BrianVu <bachvu236@gmail.com>
2023-09-17 23:43:51 +0700

    Config router and cors

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var express = require('express')
-    // cors = require('cors')
+ const express = require('express')
+ const cors = require("cors");
+ const bookRouter = require('./router/Book.router')
+ const app = express() 
 
-var app = express() 
+ const corsOptions = {
+    origin: "http://localhost:8081"
+  };
+    app.use(cors(corsOptions));
 
+    //parse request of content-type - application/json
+    app.use(express.json())
+    //parse request of content-type - application/x-www-form-urlencoded
+    app.use(express.urlencoded({extended: true}))
+    app.use('/',bookRouter);
 
-var server = app.listen(process.env.PORT || 3000, function(){
+const server = app.listen(process.env.PORT || 8080, function(){
     console.log('Listening on port ' + server.address().port);
 });
